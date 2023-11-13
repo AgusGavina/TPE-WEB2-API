105f1b5bd1157b30cf5866a52d6fefdf9d3f6c8f
francisco cotugno <franciscocotugno@gmail.com>
2023-11-13 09:35:27 -0300

    Un poco de limpieza de codigo

--- router.php
+++ router.php
@@ -5,31 +5,31 @@ require_once 'libs/Router.php';
 require_once 'app/controllers/admin.api.controller.php';
 require_once 'app/controllers/products.api.controller.php';
 
-// crea el router
+
 $router = new Router();
 
-// default route
+
 $router->setDefaultRoute('ProductsApiController', 'show404'); 
 
 //                      ENDPOINT             verbo          controller           metodo
-$router->addRoute('Administrador/Productos', 'GET', 'AdminApiController', 'getProducts'); // Consigna 2
-$router->addRoute('Administrador/Productos/:ID', 'GET', 'AdminApiController', 'getProducts'); // Consigna 4 
-$router->addRoute('Administrador/Productos', 'POST', 'AdminApiController', 'insertProduct'); // Consigna 5
-$router->addRoute('Administrador/Productos/:ID', 'PUT', 'AdminApiController', 'updateProduct'); // Consigna 5   
+$router->addRoute('Administrador/Productos', 'GET', 'AdminApiController', 'getProducts'); 
+$router->addRoute('Administrador/Productos/:ID', 'GET', 'AdminApiController', 'getProducts'); 
+$router->addRoute('Administrador/Productos', 'POST', 'AdminApiController', 'insertProduct'); 
+$router->addRoute('Administrador/Productos/:ID', 'PUT', 'AdminApiController', 'updateProduct');  
 $router->addRoute('Administrador/Productos/:ID', 'DELETE', 'AdminApiController', 'deleteProduct');
-$router->addRoute('Administrador/Categorias', 'GET', 'AdminApiController', 'getCategorys'); // Consigna 2
-$router->addRoute('Administrador/Categorias/:ID', 'GET', 'AdminApiController', 'getCategorys'); // Consigna 4 
-$router->addRoute('Administrador/Categorias', 'POST', 'AdminApiController', 'insertCategory'); // Consigna 5  
-$router->addRoute('Administrador/Categorias/:ID', 'PUT', 'AdminApiController', 'updateCategory'); // Consigna 5  
+$router->addRoute('Administrador/Categorias', 'GET', 'AdminApiController', 'getCategorys'); 
+$router->addRoute('Administrador/Categorias/:ID', 'GET', 'AdminApiController', 'getCategorys'); 
+$router->addRoute('Administrador/Categorias', 'POST', 'AdminApiController', 'insertCategory');   
+$router->addRoute('Administrador/Categorias/:ID', 'PUT', 'AdminApiController', 'updateCategory');   
 $router->addRoute('Administrador/Categorias/:ID', 'DELETE', 'AdminApiController', 'deleteCategory');
 
 
-$router->addRoute('Productos', 'GET', 'ProductsApiController', 'getProducts'); // Consigna 2
+$router->addRoute('Productos', 'GET', 'ProductsApiController', 'getProducts'); 
 $router->addRoute('Productos/:Categoria', 'GET', 'ProductsApiController', 'getProductsByParams');
-$router->addRoute('Productos/:Categoria?sort=price&order=asc', 'GET', 'ProductsApiController', 'getProductsByCategoryAsc'); // Consigna 3
+$router->addRoute('Productos/:Categoria?sort=price&order=asc', 'GET', 'ProductsApiController', 'getProductsByCategoryAsc');
 $router->addRoute('Productos/:Categoria?sort=price&order=desc', 'GET', 'ProductsApiController', 'getProductsByCategoryDesc');
 $router->addRoute('Productos/:Categoria/:Producto', 'GET', 'ProductsApiController', 'getProductByParams');
 
-// rutea
+
 $router->route($_GET['resource'], $_SERVER['REQUEST_METHOD']);
 ?>
